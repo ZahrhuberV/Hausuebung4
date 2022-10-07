a64221086cb0db4109e367665d76b145db74119c
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-10-07 10:18:54 +0200

    ReadFile implementiert

---
+++ src/com/company/ReadFile.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReadFile {
+    public ArrayList<Integer> readNumbers(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader("numbers.csv"))){
+           while(true) {
+               line = br.readLine();
+               String[] parts = line.split(":");
+               for(int i = 0; i < parts.length; i++) {
+                   try {
+                       numbers.add(Integer.parseInt(parts[i]));
+                   } catch (NumberFormatException nfe){
+                       System.out.println("Not a Number");
+                   }
+               }
+           }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e){
+            System.out.println("NullPointException");
+        }
+
+        return numbers;
+    }
+}
