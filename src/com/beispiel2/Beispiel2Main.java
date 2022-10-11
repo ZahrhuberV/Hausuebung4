92466c02c93c3f17ada58b44d3b1345e9c7be9d4
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-10-11 11:01:15 +0200

    Beispiel2 210 funkt noch nicht, 200 schon

--- src/com/beispiel2/Beispiel2Main.java
+++ src/com/beispiel2/Beispiel2Main.java
@@ -1,7 +1,5 @@
 package com.beispiel2;
 
-import com.beispeil1.DivideNumbersTask;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,29 +11,43 @@ public class Beispiel2Main {
         ArrayList<Integer> numbers = new ArrayList<>();
         Scanner s = new Scanner(System.in);
         int n;
+        int border;
         System.out.println("n>");
         n = s.nextInt();
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             numbers.add(i);
         }
 
-        final ExecutorService pool =
-                Executors.newFixedThreadPool(n/100+1);
+        final ExecutorService pool;
+
+        if(n%100 == 0){
+            pool = Executors.newFixedThreadPool(n/100);
+            border = n/100;
+        } else {
+            pool = Executors.newFixedThreadPool(n/100 +1);
+            border = n/100+1;
+        }
+
 
         final List<SumTask> callables = new ArrayList<>();
 
-        for (int i = 0; i < n/100; i++) {
-            List<Integer> chunk = numbers.subList(i * (n/100), (i+1) * n/100);
+        for (int i = 0; i < border; i++) {
+            List<Integer> chunk = numbers.subList(i * 100, (i+1) * 100);
             callables.add(new SumTask(chunk));
         }
+
+        int sumAll = 0;
 
         try {
             for (final Future<Integer> future :
                     pool.invokeAll(callables)) {
                 System.out.println(future.get());
+                sumAll = sumAll + future.get();
             }
         } catch (ExecutionException | InterruptedException ex) { }
         pool.shutdown();
+
+        System.out.println("Sum:" + sumAll);
 
     }
 }
